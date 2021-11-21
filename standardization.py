30f8ddb3c0175845b10789ac10bf74985734d00f
Lorenzo Marsicano <lorenzomarsicano@gmail.com>
2021-11-21 21:31:05 +0100

    Remove useless python script

--- standardization.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-
-data = pd.read_csv('feats_ordered_old.csv', quoting=2)
-data = data.drop(columns=['Path', '3D_A3', '3D_D1', '3D_D2','3D_D3','3D_D4'])
-mean = data.mean()
-std = data.std()
-f_data = (data-mean)/std
-print(f_data.to_csv())
